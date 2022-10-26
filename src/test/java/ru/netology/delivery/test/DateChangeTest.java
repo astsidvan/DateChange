b24a514dad40c093bafee94e0b884b99b4b720b0
astsidvan <astsidvan@gmail.com>
2022-10-26 13:32:03 +0300

    Классы проектов перенесены в тест

--- src/test/java/ru/netology/delivery/test/DateChangeTest.java
+++ src/test/java/ru/netology/delivery/test/DateChangeTest.java
@@ -1,20 +1,13 @@
 package ru.netology.delivery.test;
 
-import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import ru.netology.delivery.data.DataGenerator;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 import static java.time.Duration.ofSeconds;
-
 import static ru.netology.delivery.data.DataGenerator.*;
 
 class DeliveryTest {
@@ -63,9 +56,3 @@ class DeliveryTest {
                 .shouldBe(visible, ofSeconds(5)).shouldHave(exactText("Встреча успешно запланирована на " + secondMeetingDate));
     }
 }
-// TODO: добавить логику теста в рамках которого будет выполнено планирование и перепланирование встречи.
-// Для заполнения полей формы можно использовать пользователя validUser и строки с датами в переменных
-// firstMeetingDate и secondMeetingDate. Можно также вызывать методы generateCity(locale),
-// generateName(locale), generatePhone(locale) для генерации и получения в тесте соответственно города,
-//имени и номера телефона без создания пользователя в методе generateUser(String locale) в датагенераторе
-
